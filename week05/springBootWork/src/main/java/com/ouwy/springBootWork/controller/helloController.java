86a8044acc684c580bf01fa2d4cbab8db350f2d1
Yuan <owy965@sina.com>
2021-06-08 13:42:26 +0800

    springboot调整目录结构解决运行找不到control的问题

--- week05/springBootWork/src/main/java/com/ouwy/springBootWork/controller/helloController.java
+++ week05/springBootWork/src/main/java/com/ouwy/springBootWork/controller/helloController.java
@@ -1,4 +1,4 @@
-package com.ouwy.controller;
+package com.ouwy.springBootWork.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- week05/springBootWork/src/main/java/com/ouwy/springBootWork/mysqlController/DBMySqlController.java
+++ week05/springBootWork/src/main/java/com/ouwy/springBootWork/mysqlController/DBMySqlController.java
@@ -1,4 +1,4 @@
-package com.ouwy.mysqlController;
+package com.ouwy.springBootWork.mysqlController;
 
 import java.util.Iterator;
 import java.util.List;
